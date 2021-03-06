14c54cfc95d5cf83e63d781032dd8923a8865dd2
ichiroyamasawa <ichiro.yamasawa.iics@ust.edu.ph>
2021-03-06 21:24:22 +0800

    + ContactUs

--- React/src/Pages/ContactUs/index.js
+++ React/src/Pages/ContactUs/index.js
@@ -1,10 +1,102 @@
 import React from "react";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col, Container, Form, Button, Modal } from "react-bootstrap";
+import mapIMG from "./../../Assets/MapSample.jpg";
+import shopeeIMG from "./../../Assets/shopee-logo.png";
 
 const ContactUs = (props) => {
   return (
     <div className="contactUs">
       <h1 className="contactUs-sectionTitle">Contact Us</h1>
+      <Container className="contactUsWrapper">
+        <Row>
+          <Col>
+            <Container>
+              <Row>
+                <Col>
+                  <h4 className="contactUsDetails">
+                    <i class="fa fa-phone" aria-hidden="true"></i> Contact
+                    Number: 0997 063 2962
+                  </h4>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h4 className="contactUsDetails">
+                    <i class="fa fa-envelope" aria-hidden="true"></i> Email:
+                    penelopescollectionz@gmail.com
+                  </h4>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h4 className="contactUsDetails">Follow us on:</h4>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="followUsBtnLinks">
+                    <a
+                      href="https://www.facebook.com/penelopescollectionz"
+                      target="_blank"
+                    >
+                      <Button block size="lg">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>{" "}
+                        Facebook
+                      </Button>
+                    </a>
+                    <a
+                      href="https://www.instagram.com/penelopescollectionz/"
+                      target="_blank"
+                    >
+                      <Button block className="btn-instagram" size="lg">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>{" "}
+                        Instagram
+                      </Button>
+                    </a>
+                    <a
+                      href="https://shopee.ph/penelopes_collectionz"
+                      target="_blank"
+                    >
+                      <Button
+                        block
+                        className="btn-shopee shadow-none"
+                        size="lg"
+                      >
+                        <img
+                          className="shopee-logo"
+                          src={shopeeIMG}
+                          alt="Shopee Logo"
+                        />{" "}
+                        Shopee
+                      </Button>
+                    </a>
+                    <br />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col>
+            <Container>
+              <Row>
+                <Col>
+                  <h4 className="contactUsDetails">
+                    <i class="fa fa-location-arrow" aria-hidden="true"></i>{" "}
+                    Location:
+                  </h4>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <img src={mapIMG} alt="Map" className="contactUsMap" />
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
